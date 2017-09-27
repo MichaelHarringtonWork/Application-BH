29ae5c394e0c374ab45edfa7fbdfa16c381f7410
Michael <michael.harrington@oracle.com>
2017-09-27 14:50:18 +0100

    Add Comments & Admin
    Comments added and general code clean up, removing no longer needed code.

--- public_html/js/client-ws.js
+++ public_html/js/client-ws.js
@@ -1,6 +1,5 @@
 var ws = new WebSocket('ws://localhost:4200');
 
-
 window.onload = function () {
     var _DistanceTravelled;
     var _SpeedMPH;
@@ -24,6 +23,8 @@ window.onload = function () {
         setTitle("Disconnected from Bloodhound Dashboard");
     };
 
+//on receiving a message, depending on the sensor name, extract the sensor value and assign to a 
+//private variable so that it can be used in index.js to update components.
     ws.onmessage = function (payload) {
         console.log("This is the payload received by the client");
         console.log(payload);
@@ -33,55 +34,42 @@ window.onload = function () {
         switch(data["sensor_name"]){
             case "CLAC_Distance_Travelled" : //Distance travelled in Meters
                 _DistanceTravelled = data["sensorCalculatedValue"];
-                //document.querySelector('#DistTrav2').innerHTML = data["sensorCalculatedValue"];
                 break;
             case "RS232_P2_FUEL_GPS_mph": //Speedo in MPH
                 _SpeedMPH = data["sensorCalculatedValue"];
-                //document.querySelector('#MPH').innerHTML =  data["sensorCalculatedValue"];
                 break;
             case "CALC_DECUJetTBTPercent": //Jet Turbine Blade Temperature (percentage of measurement range)
                 _TurbinTemp = data["sensorCalculatedValue"];
-                //document.querySelector('#TurbineTemp').innerHTML = data["sensorCalculatedValue"];
                 break;
             case "MILBUS_DECUJetNozArea": //The nozzle exhaust area (Area Jet)
                 _AJ = data["sensorCalculatedValue"];
-                //document.querySelector('#AJ').innerHTML = data["sensorCalculatedValue"];
                 break;
             case "MILBUS_DECUJetNL": //Low pressure spool area
                 _NL = data["sensorCalculatedValue"];
-                //document.querySelector('#NL').innerHTML = data["sensorCalculatedValue"];
                 break;
             case "MILBUS_DECUJetNH": //High pressure spool area 
                 _NH = data["sensorCalculatedValue"];
-                //document.querySelector('#NH').innerHTML = data["sensorCalculatedValue"];
                 break;
             case "CALC_JetFuelRemaining": //Fuel Level
                 _FuelLevel = data["sensorCalculatedValue"];
-                //document.querySelector('#FuelLevel').innerHTML = data["sensorCalculatedValue"];
                 break;
             case "A1_20_FL_ACC_Z": //Lateral side-to-side G
                 _LateralG = data["sensorCalculatedValue"];
-                //document.querySelector('#LateralG').innerHTML = data["sensorCalculatedValue"];
                 break;
             case "A2_2_FR_TC2_OUTER_BRK_PAD_TEMP": //Brake pad temps front 
                 _BreakPadTempFront = data["sensorCalculatedValue"];
-                //document.querySelector('#BreakPadTempFront').innerHTML = data["sensorCalculatedValue"];
                 break;
             case "A2_2_BR_TC2_OUTER_BRK_PAD_TEMP": //Break pads temp back
                 _BreakPadTempBack = data["sensorCalculatedValue"];
-                 //document.querySelector('#BreakPadTempBack').innerHTML = data["sensorCalculatedValue"];
                 break;
             case "A2_26_POWER_BUS_VOLTAGE_SIG": //Power Bus Voltage
                 _PowerBusVoltage = data["sensorCalculatedValue"];
-                //document.querySelector('#PowerBusVoltage').innerHTML = data["sensorCalculatedValue"];
                 break;
             case "CALC_MachNo": //Mach No.
                 _MachNo = data["sensorCalculatedValue"];
-                //document.querySelector('#MachNo').innerHTML = data["sensorCalculatedValue"];
                 break;
             case "CALC_SpeedOfSound": //Speed Of Sound
                 _SpeedOfSound = data["sensorCalculatedValue"];
-                //document.querySelector('#SpeedOfSound').innerHTML = data["sensorCalculatedValue"];
                 break;
         };
    
--- public_html/js/ws.js
+++ public_html/js/ws.js
@@ -1,29 +1,30 @@
 var kafka = require('kafka-node');
 var Consumer = kafka.Consumer,
-        client = new kafka.Client('129.146.69.162:9092'),
+        client = new kafka.Client('129.146.69.162:9092'), //Oracle Bloodhound Kafka bootstrap server address  
         consumer = new Consumer(
-        client, [{topic: 'bhdata', partition: 0}], {autoCommit: false});
+        client, [{topic: 'bhdata', partition: 0}], {autoCommit: false}); //specify the topic and partition.
 
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var app = express();
 var BHDATA;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("./public_html"));
 app.use(cors());
 
-consumer.on('connection', function(){
+consumer.on('connection', function(){ //log when a connection to the kafka server is established
    console.log('Connection to Kafka established'); 
 });
 
 consumer.on('message', function(message){
-    var bhdata = JSON.parse(message.value);
+    var bhdata = JSON.parse(message.value); //get kafka message and parse to a variable
     console.log("KAFKA SAYS...");
     console.log(bhdata);
-    this.BHDATA = bhdata;
+    this.BHDATA = bhdata; //assign data to a private variable so that it can be accessed elsewhere in the file.
     sendData(this.BHDATA);
 });
 
@@ -31,28 +32,15 @@ consumer.on('error', function(err){
     console.log('error', err);
 });
 
-//app.post('/bhdata', function (request, response, next) {
-//    console.log("Data received");
-//    var bhdata = request.body;
-//    this.BHDATA = bhdata;
-    //console.log(bhdata);
-//    response.send("OK");
-    
-//    sendData(this.BHDATA);
-//    next();
-//});
-
 function sendData(bhdata) {
     wss.broadcast(JSON.stringify(bhdata));
 };
 
-//const StreamAPort = 4201;
-//app.listen(StreamAPort);
 console.log("Server is listening for a kafka topic");
 
-const clientPort = 4200;
+const clientPort = 4200; 
 WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({port: clientPort});
+var wss = new WebSocketServer({port: clientPort}); //create a websocket for clients to connect to
 
 wss.on("connection", function (ws) {
     ws.send("Welcome to the Bloodhound Dashboard");
@@ -60,7 +48,7 @@ wss.on("connection", function (ws) {
 });
 
 wss.broadcast = function (bhdata) {
-    wss.clients.forEach(function (client) {
+    wss.clients.forEach(function (client) { //broadcast data received from kafka to all clients connected.
         client.send(bhdata);
         console.log("Data sent");
     });
