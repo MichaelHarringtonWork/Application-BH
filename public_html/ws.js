1d836a60e33f8bde780c89179b70f7cbd542be8c
Michael <michael.harrington@oracle.com>
2017-09-27 11:34:40 +0100

    Remove websocket post listener
    Remove Websocket listener for a /Post request from OSA. Replaced with Kafka consumer code

--- public_html/ws.js
+++ public_html/ws.js
@@ -1,3 +1,9 @@
+var kafka = require('kafka-node');
+var Consumer = kafka.Consumer,
+        client = new kafka.Client('129.146.69.162:9092'),
+        consumer = new Consumer(
+        client, [{topic: 'bhdata', partition: 0}], {autoCommit: false});
+
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
@@ -9,25 +15,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("./public_html"));
 app.use(cors());
 
-
-app.post('/bhdata', function (request, response, next) {
-    console.log("Data received");
-    var bhdata = request.body;
-    this.BHDATA = bhdata;
-    //console.log(bhdata);
-    response.send("OK");
-    
-    sendData(this.BHDATA);
-    next();
+consumer.on('connection', function(){
+   console.log('Connection to Kafka established'); 
 });
+
+consumer.on('message', function(message){
+    var bhdata = JSON.parse(message.value);
+    console.log("KAFKA SAYS");
+    console.log(bhdata);
+    this.BHDATA = bhdata;
+    sendData(this.BHDATA);
+});
+
+consumer.on('error', function(err){
+    console.log('error', err);
+});
+
+//app.post('/bhdata', function (request, response, next) {
+//    console.log("Data received");
+//    var bhdata = request.body;
+//    this.BHDATA = bhdata;
+    //console.log(bhdata);
+//    response.send("OK");
+    
+//    sendData(this.BHDATA);
+//    next();
+//});
 
 function sendData(bhdata) {
     wss.broadcast(JSON.stringify(bhdata));
 };
 
-const StreamAPort = 4201;
-app.listen(StreamAPort);
-console.log("Server is listening to port 4201");
+//const StreamAPort = 4201;
+//app.listen(StreamAPort);
+console.log("Server is listening for a kafka topic");
 
 const clientPort = 4200;
 WebSocketServer = require('ws').Server;
